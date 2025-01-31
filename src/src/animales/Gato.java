45603c6962be94c2225d8879096a503ac3f941e7
Andres-star1 <felipe.kafe05@gmail.com>
2025-01-30 21:57:09 -0500

    Add files via upload

---
+++ src/src/animales/Gato.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package animales;
+
+
+public class Gato extends Felino {
+    public String getSonido() {
+    return "Maullido";
+     }
+    public String getAlimentos() {
+    return "ratones";
+     }
+    public String getHabitat() {
+    return "domestico";
+     }
+    public String getNombreCientifico() {
+    return "Felis silvestris catus";
+     }
+}
